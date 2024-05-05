feb580c455c815fb037f537851c1bcc06b4a0145
rosyid <rajamasker@gmail.com>
2024-05-05 19:12:08 +0900

    add card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Card from "@/components/card";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[url('../public/assets/pattern-background-mobile.svg')] bg-cover bg-no-repeat bg-primary-pale-blue md:bg-[url('../public/assets/pattern-background-desktop.svg')] md:bg-contain">
-      
+      <Card/>
     </main>
   );
 }
